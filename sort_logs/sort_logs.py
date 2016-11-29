d3d0d16b21c0dc7912a1d98fd24519532c84ed01
Adamo <adamobr@gmail.com>
2016-11-29 15:45:00 -0200

    script to parse logs

---
+++ sort_logs/sort_logs.py
@@ -0,0 +1,40 @@
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--sort", "-s", type=str, required=False)
+parser.add_argument("--limit", "-l", type=int, required=False, default=10)
+
+args = parser.parse_args()
+
+
+if args.sort in ['nMatched', 'nModified', 'ninserted','nscanned'] : print 'OK'
+
+strfilter = args.sort
+limit = args.limit
+
+output = []
+total  = 0
+
+for line in sys.stdin:
+    position = line.find(strfilter)
+    total = total + 1
+    if position > 0:
+        next_space = line.find(' ',position)
+        metric = line[position+len(strfilter)+1:next_space]
+        output.append({'logline' : line, 'value' : metric})
+    if total > limit * 2: #perform a sort and remove part of the array
+        output = sorted(output, key=lambda k: int(k.get('value', 0)), reverse=True)
+        output = output[:10]
+        total = 0
+
+output = sorted(output, key=lambda k: int(k.get('value', 0)), reverse=True)
+output = output[:limit]
+
+
+for i in output:
+    print  '(' + i['value'] + ')' + '  --log--> ' + i['logline']
+    print '--------------------------------'
+
+
