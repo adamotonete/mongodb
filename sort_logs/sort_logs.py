a670d33beba142d58d1b917414db2ac96495ae4e
Adamo Tonete <5470767+adamotonete@users.noreply.github.com>
2022-07-14 23:21:54 -0300

    fix for python3

--- sort_logs/sort_logs.py
+++ sort_logs/sort_logs.py
@@ -8,10 +8,10 @@ parser.add_argument("--limit", "-l", type=int, required=False, default=10)
 
 args = parser.parse_args()
 
-valid_sorts = ['reslen','nreturned','nMatched', 'nModified', 'docsExamined', 'ninserted','nscanned', 'nscannedObjects']
+valid_sorts = ['keysExamined','reslen','nreturned','nMatched', 'nModified', 'docsExamined', 'ninserted','nscanned', 'nscannedObjects']
 if args.sort not in valid_sorts:
-    print 'Invalid sort, please use one of: ' + ', '.join(str(x) for x in valid_sorts)
-    exit
+    print('Invalid sort, please use one of: ' + ', '.join(str(x) for x in valid_sorts))
+    exit()
 
 strfilter = args.sort
 limit = args.limit
@@ -25,8 +25,8 @@ for line in sys.stdin:
     if position > 0:
         next_space = line.find(' ',position)
         metric = line[position+len(strfilter)+1:next_space]
-        if metric <> '':
-	    output.append({'logline' : line, 'value' : metric})
+        if metric != '':
+            output.append({'logline' : line, 'value' : metric})
     if total > limit * 2: #perform a sort and remove part of the array
         output = sorted(output, key=lambda k: int(k.get('value', 0)), reverse=True)
         output = output[:limit]
@@ -37,7 +37,5 @@ output = output[:limit]
 
 
 for i in output:
-    print  '(' + i['value'] + ')' + '  --log--> ' + i['logline']
-    print '--------------------------------'
-
-
+    print(  '(' + i['value'] + ')' + '  --log--> ' + i['logline'])
+    print( '--------------------------------')
